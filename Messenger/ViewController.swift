06d2826d2d2288c6638baa2b9c5f8b315fb57788
James Thang <jamesthang1996@gmail.com>
2021-08-07 13:41:52 +0700

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by James Thang on 07/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
